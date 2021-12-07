830adaa0263d62c689c1af9aa847e3799d3a4ba4
xjx <jingyanyi936@163.com>
2021-12-07 20:35:53 +0800

    second commit

--- src/main/java/cn/edu/ncu/xjx/git/GitTest.java
+++ src/main/java/cn/edu/ncu/xjx/git/GitTest.java
@@ -3,5 +3,6 @@ package cn.edu.ncu.xjx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello!git!");
+        System.out.println("hello!git2");
     }
 }
